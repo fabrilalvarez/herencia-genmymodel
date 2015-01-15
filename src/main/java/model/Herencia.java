38640be064431e00b7dcd3007797d6555bdafe94
fabrilalvarez <fabrilalvarez@eniac23.danielcastelao.org>
2015-01-15 09:54:32 +0100

    todo ok

---
+++ src/main/java/model/Herencia.java
@@ -0,0 +1,25 @@
+package model;
+
+
+/**
+ * Clase principal main
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Herencia
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+
+	
+	public static void main() {
+		// TODO implement me	
+	}
+	
+}
+
